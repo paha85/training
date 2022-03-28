cb36b5dc09578b5a9c802828ea58f176069fdab9
paha85 <padisb@gmail.com>
2022-03-28 20:22:22 +0300

    Задание 2,3

---
+++ hw2_task2.py
@@ -0,0 +1,29 @@
+time = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+for item in time:
+    if item.isdigit() or item[1:].isdigit():
+        print(time.index(item))
+#         item = [str(item).zfill(2) for item in time]
+# print(time)
+        # time.str(item).zfill(2) for item in digitime
+        # digitime.append(item)
+
+
+# s = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+# print(" ".join(map(lambda x: '"%s"' % __import__('re').sub('\d+', lambda x: f' { x[0] } '.zfill(2), x) if any(map(str.isdigit, x)) else x, s)))
+# tutime = ()
+
+# for item in time:
+#     tutime.append(item)
+# print(tutime)
+    # time.insert((1,3) '"')
+# print(time)
+# print(time.zfill(2))
+# time_dva = []
+# for kimchi in time:
+#     if int(kimchi) == type(str):
+#         kimchi =
+# print(time)
+# text = ' '.join(time)
+# print(text)
+# time = [str(item).zfill(2) for item in time]
+# print(time)
