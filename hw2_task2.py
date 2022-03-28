98b149464907141a898989b8154ac4189122df9c
paha85 <padisb@gmail.com>
2022-03-28 22:50:40 +0300

    ДЗ

--- hw2_task2.py
+++ hw2_task2.py
@@ -1,29 +1,13 @@
 time = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+kimchi = []
 for item in time:
-    if item.isdigit() or item[1:].isdigit():
-        print(time.index(item))
-#         item = [str(item).zfill(2) for item in time]
-# print(time)
-        # time.str(item).zfill(2) for item in digitime
-        # digitime.append(item)
-
-
-# s = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-# print(" ".join(map(lambda x: '"%s"' % __import__('re').sub('\d+', lambda x: f' { x[0] } '.zfill(2), x) if any(map(str.isdigit, x)) else x, s)))
-# tutime = ()
-
-# for item in time:
-#     tutime.append(item)
-# print(tutime)
-    # time.insert((1,3) '"')
-# print(time)
-# print(time.zfill(2))
-# time_dva = []
-# for kimchi in time:
-#     if int(kimchi) == type(str):
-#         kimchi =
-# print(time)
-# text = ' '.join(time)
-# print(text)
-# time = [str(item).zfill(2) for item in time]
-# print(time)
+    if item.isdigit():
+        item = item.zfill(2)
+        kimchi.append(f'"{item}"')
+    elif item[1:].isdigit():
+        item = item.zfill(3)
+        kimchi.append(f'"{item}"')
+    else:
+        kimchi.append(item)
+print(' '.join(kimchi))
