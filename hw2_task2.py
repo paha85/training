dfa4a8288018043ee3ce2d84c18bf2f37e44f24e
paha85 <padisb@gmail.com>
2022-03-29 07:52:13 +0300

    ДЗ

--- hw2_task2.py
+++ hw2_task2.py
@@ -4,10 +4,22 @@ kimchi = []
 for item in time:
     if item.isdigit():
         item = item.zfill(2)
-        kimchi.append(f'"{item}"')
+        kimchi.append(item)
+        # kimchi.append(f'"{item}"')
+        kimchi.insert(kimchi.index(item), '"')
+        kimchi.insert(kimchi.index(item) + 2, '"')
     elif item[1:].isdigit():
         item = item.zfill(3)
-        kimchi.append(f'"{item}"')
+        # kimchi.append(f'"{item}"')
+        kimchi.append(item)
+        kimchi.insert(kimchi.index(item), '"')
+        kimchi.insert(kimchi.index(item) + 2, '"')
     else:
         kimchi.append(item)
-print(' '.join(kimchi))
+print(kimchi)
+
+kimchi[1:4] = [''.join(kimchi[1:4])]
+kimchi[3:6] = [''.join(kimchi[3:6])]
+kimchi[8:11] = [''.join(kimchi[8:11])]
+kimchi = ' '.join(kimchi)
+print(f'{kimchi}')
